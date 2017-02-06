7b7d47998fbdf3b271e4264b1108c07123c53220
JulienBe <bertozzijulien@gmail.com>
2017-02-06 22:09:54 +0100

    Random pipe and removed container

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -3,20 +3,20 @@ extends Node
 export var amount_to_fill_view = 0
 export var scn = preload("res://scenes/pipe.tscn")
 export var padding = 0
+export var min_y = 0
+export var max_y = 0
+var cpt = 0
 
 func _ready():
 	for i in range(amount_to_fill_view):
-		spawn_and_update_pos()
-
-func spawn_and_update_pos():
-	spawn()
-	go_next_pos()
+		spawn()
 
 func spawn():
+	print(cpt)
 	var new = scn.instance()
-	new.set_pos(get_pos())
-	new.connect("exit_tree", self, "spawn_and_update_pos")
-	get_node("container").add_child(new)
-
-func go_next_pos():
-	set_pos(get_pos() + Vector2(padding, 0))
+	var rnd = rand_range(min_y, max_y)
+	var pos = Vector2(padding * cpt, rnd)
+	new.set_pos(pos)
+	new.connect("exit_tree", self, "spawn")
+	add_child(new)
+	cpt += 1
